0fff1bdf726207f34f68f626ab3a96e383e89fa4
Bruce Hubbard <brucehubbard@gmail.com>
2016-10-06 10:11:28 -0400

    Bumping version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'differential:cluster',
   summary: 'Start headless meteor processes.',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/Differential/meteor-cluster'
 });
 
